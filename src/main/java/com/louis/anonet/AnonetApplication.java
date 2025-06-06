8f17af033221d823f4505efe24487aa892993644
Luis3Fernando <luisfernando3chr@gmail.com>
2025-06-06 16:12:11 -0500

    spring initializer

---
+++ src/main/java/com/louis/anonet/AnonetApplication.java
@@ -0,0 +1,13 @@
+package com.louis.anonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/louis/anonet/AnonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louis.anonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
